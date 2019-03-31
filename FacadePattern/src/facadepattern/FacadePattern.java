65282a74cc40215ccb92d57754f9be0e47c41709
Fabio Maksimchuk <fabiodmk@gmail.com>
2019-03-31 11:34:06 -0300

    Creación proyecto java con netbeans del ejemplo patron facade

---
+++ FacadePattern/src/facadepattern/FacadePattern.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facadepattern;
+
+/**
+ *
+ * @author fabm_
+ */
+public class FacadePattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Main del ejemplo del patron Facade
+        System.out.println("Desarrollando el ejemplo del patrón Facade para los alumnos");
+        
+    }
+    
+}
